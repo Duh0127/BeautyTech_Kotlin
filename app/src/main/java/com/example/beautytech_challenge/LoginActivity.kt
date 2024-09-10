a67ceef0fb2ad75c083ea7925b3e0a979d8812a1
Duh0127 <rodatupmocgamer22@gmail.com>
2024-09-10 17:50:35 -0300

    init

---
+++ app/src/main/java/com/example/beautytech_challenge/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.beautytech_challenge
+
+import android.app.Activity
+import android.os.Bundle
+
+class LoginActivity : Activity() {
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/beautytech_challenge/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.beautytech_challenge
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+
+
+
+    }
+
+
+}
