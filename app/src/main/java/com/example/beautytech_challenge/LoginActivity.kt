e084f10d04b43ddca9bc616a62f1f314d4a83a43
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-10 22:42:49 -0300

    login, register and profile activities

--- app/src/main/java/com/example/beautytech_challenge/LoginActivity.kt
+++ app/src/main/java/com/example/beautytech_challenge/LoginActivity.kt
@@ -1,14 +1,30 @@
 package com.example.beautytech_challenge
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : Activity() {
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
 
+        setContentView(R.layout.login_activity)
 
+        val registerButton = findViewById<Button>(R.id.btn_register)
 
+        // Configure o listener de clique para o botão
+        registerButton.setOnClickListener {
+            // Crie um Intent para iniciar a LoginActivity
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        val mainButton = findViewById<Button>(R.id.btn_back)
+        mainButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
--- app/src/main/java/com/example/beautytech_challenge/RegisterActivity.kt
+++ app/src/main/java/com/example/beautytech_challenge/RegisterActivity.kt
@@ -5,28 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : Activity() {
-
+class RegisterActivity : Activity() {
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
 
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.register_layout)
 
-        // Encontre o botão com o ID btn_login
-        val loginButton: Button = findViewById(R.id.btn_login)
+        val backButton = findViewById<Button>(R.id.btn_back)
 
         // Configure o listener de clique para o botão
-        loginButton.setOnClickListener {
+        backButton.setOnClickListener {
             // Crie um Intent para iniciar a LoginActivity
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-
-
-
-
-
-
     }
-
 }
